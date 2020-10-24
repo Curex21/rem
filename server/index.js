f39f58979090b0f9e1bab788d9adf971ae561e5c
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-10-24 18:12:15 -0500

    fixed yarn start

--- server/index.js
+++ server/index.js
@@ -1,31 +1,29 @@
-'use strict'
+var app = require("express")(),
+    http = require("http").createServer(app),
+    io = require("socket.io")(http),
+    port = process.env.PORT || 3000, //asigno un puerto de entrada a la conexion
+    publicDir = `${__dirname}/public`; // direccion del directorio de los html cliente y servidor
 
-var app = require('express')(),
-	http = require('http').createServer(app),
-	io = require('socket.io')(http),
-	port = process.env.PORT || 3000,     //asigno un puerto de entrada a la conexion
-	publicDir = `${__dirname}/public`   // direccion del directorio de los html cliente y servidor
-
-  // servidor web escucha el puerto y envia mensaje de ejecución
-  http.listen(port, () => {
-	console.log('Iniciando Express y Socket.IO en localhost:%d', port)
-})
+// servidor web escucha el puerto y envia mensaje de ejecución
+http.listen(port, () => {
+    console.log("Iniciando Express y Socket.IO en localhost:%d", port);
+});
 
 // aplicación
-  app
-  // ruta de ciente , usuaros para ver el streaming
-  	.get('/', (req, res) => {
-      //
-  		res.sendFile(`${publicDir}/client.html`)
-  	})
-  // ruta servidor, emisión de video por parte de usuarios
-  	.get('/streaming', (req, res) => {
-  		res.sendFile(`${publicDir}/server.html`)
-  	})
+app
+    // ruta de ciente , usuaros para ver el streaming
+    .get("/", (req, res) => {
+        //
+        res.sendFile(`${publicDir}/client.html`);
+    })
+    // ruta servidor, emisión de video por parte de usuarios
+    .get("/streaming", (req, res) => {
+        res.sendFile(`${publicDir}/server.html`);
+    });
 // socket.io
-io.on('connection', (socket) => {
-	socket.on('streaming', (image) => {
-		io.emit('play stream', image)
-		//console.log(image)
-	})
-})
+io.on("connection", (socket) => {
+    socket.on("streaming", (image) => {
+        io.emit("play stream", image);
+        //console.log(image)
+    });
+});
